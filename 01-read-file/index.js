d7c140db2e764c9a32a9e3709391564006033a1a
SadJoeBright <sadjoebright.gmail.com>
2023-05-05 02:54:38 +0300

    feat: task 01 has been solved

---
+++ 01-read-file/index.js
@@ -0,0 +1,7 @@
+const path = require('path');
+const fs = require('fs');
+const readableStream = fs.createReadStream(path.join(__dirname,'text.txt'), 'utf-8');
+let data = '';
+readableStream.on('data', chunk => data += chunk);
+readableStream.on('end', () => console.log(data));  
+// console.log(path.join(__dirname,'text.txt')); 
